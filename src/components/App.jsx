a980b2f53d9482eecf0960e7620caf40e68e09fc
Nevin <n.boyd1@yahoo.com>
2025-10-15 19:38:50 -0700

    Moved fetch logic to TaskContext for global management

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 import TaskForm from "./TaskForm";
 import SearchBar from "./SearchBar";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:6001/tasks')
-    .then(r=>r.json())
-    .then(data=>setTasks(data))
-    
-  }, []);
+  // pull array from TaskContext
+  const { tasks } = useContext(TaskContext);
 
   return (
-    <div>
+    <div className="container">
       <h1>Task Manager</h1>
       <TaskForm />
       <SearchBar />
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+              {task.title}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
