b65030fecab6c31991dd1e44aa035234c7b8636d
Nevin <n.boyd1@yahoo.com>
2025-10-15 19:41:26 -0700

    Implemented useId and useContext for accessibility and global addTask

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,19 +1,28 @@
 import React, { useState, useId, useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
+// add new tasks globally
 function TaskForm() {
   const [taskName, setTaskName] = useState("");
 
+  // provides unique id for accessibility
+  const inputId = useId();
+  const { addTask } = useContext(TaskContext);
+
+  // handles submissions   
   function handleSubmit(e) {
     e.preventDefault();
     if (taskName.trim() === "") return;
+
+    addTask(taskName);
     setTaskName("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>New Task:</label>
+      <label htmlFor={inputId}>New Task:</label>
       <input
+        id={inputId}
         type="text"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
