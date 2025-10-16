20c73417beb0388f5bc057f8e6f14f8d8d81cc4e
Nevin <n.boyd1@yahoo.com>
2025-10-16 00:00:46 -0700

    Add addTask and toggleTask to TaskContext for task creation + toggling

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, use } from "react";
 
 // create context
 const TaskContext = createContext();
@@ -14,13 +14,38 @@ function TaskProvider({ children }) {
             .catch((err) => console.error("Error fetching tasks:"));
     }, []);
 
+    // add new task to list and backend
+    function addTask(title) {
+        const newTask = {
+            title,
+            completed: false
+        };
+
+        fetch("http://localhost:6001/tasks", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newTask),
+        })
+            .then((r) => r.json())
+            .then((data) => setTasks([...tasks, data]))
+            .catch((err) => console.error("Error adding task:", err));
+    }
+
+    // toggle completion for existing task  
+    function toggleTask(id) {
+        const updatedTasks = tasks.map((task) =>
+            task.id === id ? { ...task, completed: !task.completed } : task
+        );
+        setTasks(updatedTasks);
+    }
+
+    // shared access to tasks and functions  
     return (
         <TaskContext.Provider value={{ tasks }}>
             {children}
         </TaskContext.Provider>
     );
 }
-
 
 export { TaskContext, TaskProvider };
 
